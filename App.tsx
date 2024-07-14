a37f1b60a5a0894a9258757f1375c7a515954e4e
stella <khk9562@naver.com>
2024-07-14 16:47:19 +0900

    fix: Resolve merge conflict

--- App.tsx
+++ App.tsx
@@ -1,32 +1,35 @@
 //로그인이 되어있을 때 페이지 안했을 때의 조건부 렌더링 해야함..
 
 import React from 'react';
-import { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import WelcomeScreen from './src/pages/Home/onboarding';
 import SignUpScreen from './src/pages/SignUp/SignUp';
-import Login from './src/pages/Login/Login';
+import Login from './src/pages/login/Login';
 
 const Stack = createStackNavigator();
 
-
 const App = () => {
-    //const [IsSignedIn, setIsSignedIn] = useState(false);
+  //const [IsSignedIn, setIsSignedIn] = useState(false);
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="Welcome">
-                <Stack.Screen name="  " component={WelcomeScreen} //options={{ headerShown: false }} 
-                />
-                <Stack.Screen name="Login" component={Login}  />
-                
-                
-                <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
-            
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="  "
+          component={WelcomeScreen} //options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Login" component={Login} />
+
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
