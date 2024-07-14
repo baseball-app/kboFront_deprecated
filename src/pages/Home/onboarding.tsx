21dabd0066f63557012e6e0eca0c29dc7ba56172
stella <khk9562@naver.com>
2024-07-14 18:59:51 +0900

    remove: 주석 삭제

--- src/pages/Home/onboarding.tsx
+++ src/pages/Home/onboarding.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {View, Text, Button} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
-// import TempNavigationButton from '../../components/TempNavigationButton';
 import TempNavigationButton from '@/components/TempNavigationButton';
 
 type RootStackParamList = {
